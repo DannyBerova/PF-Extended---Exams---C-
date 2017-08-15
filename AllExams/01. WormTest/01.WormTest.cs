72c4ac8f4f00b74e430df445c5560a075725d604
DannyBerova <danny_jordanova@abv.bg>
2017-08-15 22:50:01 +0300

    Combined Solution of old exams - Programming Fundamentals Extended 100/100

---
+++ AllExams/01. WormTest/01.WormTest.cs
@@ -0,0 +1,25 @@
+﻿namespace _01.WormTest
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            uint lengthInCentimeters = uint.Parse(Console.ReadLine()) * 100;
+            double widthInCentimeters = double.Parse(Console.ReadLine());
+
+            double remainder = lengthInCentimeters % widthInCentimeters;
+
+            if (remainder == 0 || widthInCentimeters == 0)
+            {
+                double calculated = lengthInCentimeters * widthInCentimeters;
+                Console.WriteLine($"{calculated:f2}");
+            }
+            else
+            {
+                double result = (lengthInCentimeters / widthInCentimeters) * 100;
+                Console.WriteLine($"{result:f2}%");
+            }
+        }
+    }
+}
