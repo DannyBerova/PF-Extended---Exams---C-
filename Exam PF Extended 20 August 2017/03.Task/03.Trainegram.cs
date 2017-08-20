c7a0276f4e2a87597fe64f8e0bb07a614c183706
Danny Berova <danny.jordanova@gmail.com>
2017-08-20 23:29:59 +0300

    Exam PF Extended 20 Aug 2017 - 360/400, 04.Trainland - 60/100;

---
+++ Exam PF Extended 20 August 2017/03.Task/03.Trainegram.cs
@@ -0,0 +1,31 @@
+﻿namespace _03.Task
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string pattern = @"^(\<\[[^A-Za-z0-9]*\][\.]{1})+([\.]{1}\[[A-Za-z0-9]*\][\.]{1})*$";
+
+            Regex regex = new Regex(pattern);
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "Traincode!")
+            {
+                Match train = regex.Match(inputLine);
+
+                if (train.Success)
+                {
+                    Console.WriteLine(train.Value.ToString());
+                }
+
+                inputLine = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exam PF Extended 20 August 2017/04.Task/04.Trainlands.cs
@@ -0,0 +1,100 @@
+﻿namespace _04.Task
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Wagon
+    {
+        public string Name { get; set; }
+
+        public long Power { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, List<Wagon>> trains = new Dictionary<string, List<Wagon>>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "It's Training Men!")
+            {
+                string[] currentCommand = inputLine
+                    .Split(new string[] { " -> ", " : "}, StringSplitOptions.None);
+
+                
+                if (currentCommand.Length > 2)
+                {
+                    string trainName = currentCommand[0];
+                    string wagonName = currentCommand[1];
+                    long wagonPower = long.Parse(currentCommand[2]);
+
+                    Wagon newWagon = new Wagon
+                    {
+                        Name = wagonName,
+                        Power = wagonPower
+                    };
+
+                    if (!trains.ContainsKey(trainName))
+                    {
+                        trains[trainName] = new List<Wagon>();
+                    }
+
+                    trains[trainName].Add(newWagon);
+                }
+                else if (currentCommand.Length == 1)
+                {
+                    string[] equals = inputLine
+                    .Split(new string[] { " = " }, StringSplitOptions.None);
+
+                    string firstTrain = equals[0];
+                    string secondTrain = equals[1];
+
+                    if (!trains.ContainsKey(firstTrain))
+                    {
+                        trains[firstTrain] = new List<Wagon>();
+                    }
+
+                    //List<Wagon> toAdd = trains[secondTrain];
+
+                    trains[firstTrain].Clear();
+                    trains[firstTrain].AddRange(trains[secondTrain]);
+                }
+                else if (currentCommand.Length == 2)
+                {
+                    string firstTrain = currentCommand[0];
+                    string secondTrain = currentCommand[1];
+
+                    if (!trains.ContainsKey(firstTrain))
+                    {
+                        trains[firstTrain] = new List<Wagon>();
+                    }
+
+                    //List<Wagon> toAdd = trains[secondTrain];
+                    trains[firstTrain].AddRange(trains[secondTrain]);
+                    trains.Remove(secondTrain);
+                }
+                
+                inputLine = Console.ReadLine();
+            }
+
+            var sortedTrains = trains
+                .OrderByDescending(t => t.Value.Sum(p => p.Power))
+                .ThenBy(t => t.Value.Count);
+
+            foreach (var train in sortedTrains)
+            {
+                Console.WriteLine($"Train: {train.Key}");
+
+                var wagons = train.Value;
+
+                foreach (var wagon in wagons.OrderByDescending(w => w.Power))
+                {
+                    Console.WriteLine($"###{wagon.Name} - {wagon.Power}");
+                }
+            }
+        }
+    }
+}
